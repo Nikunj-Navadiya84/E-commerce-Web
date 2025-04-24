2f69d7c777f11f0a36939aa6cdfe47d610b55835
Nikunj-Navadiya <nikunjnavadiya7@gmail.com>
2025-04-24 14:01:52 +0530

    add

--- server/server.js
+++ server/server.js
@@ -53,11 +53,3 @@ app.use("/api/address", AddressRoutes)
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-
-const path = require('path');
-
-app.use(express.static(path.join(__dirname, 'client', 'build')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-});
